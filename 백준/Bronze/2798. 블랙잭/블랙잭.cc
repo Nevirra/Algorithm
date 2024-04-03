efdc22d0fac3bfd21f0972130ace83c8d60d15ba
Nevirra <149706642+Nevirra@users.noreply.github.com>
2024-04-03 18:15:48 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -0,0 +1,26 @@
+//배우지 않은 탐색 알고리즘이라 어떻게 구현해야할지 모르겠습니다.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	int card;
+	int maxSet = 0;
+	for (int i = 0; i < n - 2; i++) {
+		for (int j = i + 1; j < n - 1; j++) {
+			for (int k = j + 1; k < n; k++) {
+				card = arr[i] + arr[j] + arr[k];
+				if (card <= m && maxSet < card) maxSet = card;
+			}
+		}
+	}
+	cout << maxSet;
+	return 0;
+}
