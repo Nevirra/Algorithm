60caae9737fb634f412d293ea0dd145c4878a9ea
Nevirra <149706642+Nevirra@users.noreply.github.com>
2024-05-01 16:38:08 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -2,18 +2,35 @@
 #include <algorithm>
 using namespace std;
 
+void Sort(int arr[], int N) {
+	int i, j, index, temp;
+	for (i = 0; i < N - 1; i++) {
+		index = i;
+		for (j = i + 1; j < N; j++) {
+			if (arr[j] < arr[index]) {
+				index = j;
+			}
+		}
+		temp = arr[index];
+		arr[index] = arr[i];
+		arr[i] = temp;
+	}
+}
+
 int main() {
-    int N;
-    scanf("%d", &N);
+	int N;
+	scanf("%d", &N);
 
-    int integer[1000];
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &integer[i]);
-    }
+	int integer[1000];
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &integer[i]);
+	}
 
-    sort(integer, integer + N);
+	Sort(integer, N);
 
-    for (int i = 0; i < N; i++) {
-        printf("%d\n", integer[i]);
-    }
+	for (int i = 0; i < N; i++) {
+		printf("%d\n", integer[i]);
+	}
+
+	return 0;
 }
